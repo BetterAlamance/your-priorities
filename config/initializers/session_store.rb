efbb32cb1b3c059492910e60e51fc736d3efe9ba
Derek Lackaff <lackaff@lackaff.net>
2014-04-21 14:45:45 -0400

    revert non-heroku change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,7 +7,7 @@ begin
   elsif ENV['YRPRI_ALL_DOMAIN']
     YourPriorities::Application.config.session_store :cookie_store, key: "_#{Instance.first.domain_name.gsub(".","_")}_production_all_domain", :domain => ".#{Instance.first.domain_name}"
   else
-	YourPriorities::Application.config.session_store :cookie_store, key: "_my_app_production"  
+    YourPriorities::Application.config.session_store :cookie_store, key: "_#{Instance.first.domain_name.gsub(".","_")}_production"
   end
 end
 
@@ -15,3 +15,4 @@ end
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # YourPriorities::Application.config.session_store :active_record_store
+
